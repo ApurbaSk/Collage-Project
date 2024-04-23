1ff29d58604851ffcc8ad273df662d700ac806ec
ApurbaSk <167886323+ApurbaSk@users.noreply.github.com>
2024-04-23 19:26:29 +0530

    Add files via upload

---
+++ Front/db.php
@@ -0,0 +1,12 @@
+<?php
+    echo "<pre>";
+        print_r($_REQUEST);
+    echo "</ptr>";
+    echo$_REQUEST['dname']."<br>";
+    echo$_REQUEST['syear']."<br>";
+    echo$_REQUEST['ssnum']."<br>";
+    echo$_REQUEST['esnum']."<br>";
+    echo$_REQUEST['Subject']."<br>";
+    echo$_REQUEST['date']."<br>";
+    echo$_REQUEST['Room']."<br>";
+    ?>
